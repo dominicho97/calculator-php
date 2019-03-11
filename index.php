79a8dd5a7932b04ed374163c536e8f510d7229bc
dominicho97 <dominic.ho97@gmail.com>
2019-03-11 13:54:59 +0100

    added operator with options

--- index.php
+++ index.php
@@ -10,6 +10,13 @@
   <form>
   <input type="text" name="num1" placeholder="Number 1">
   <input type="text" name="num2" placeholder="Number 2">
+  <select name="operator">
+  <option>None</option>
+  <option>Add</option>
+  <option>Subtract</option>
+  <option>Multiply</option>
+  <option>Divide</option>
+   </select>
   </form>
 </body>
 </html>
