836e8292dfa3a9a81f13c17b973d5c607f1cf862
dominicho97 <dominic.ho97@gmail.com>
2019-03-11 13:47:22 +0100

    added input 'number1,2

--- index.php
+++ index.php
@@ -7,6 +7,9 @@
   <title>PHP Calculator</title>
 </head>
 <body>
-  
+  <form>
+  <input type="text" name="num1" placeholder="Number 1">
+  <input type="text" name="num2" placeholder="Number 2">
+  </form>
 </body>
 </html>
